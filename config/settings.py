346fea1c65a530dfaa036355fa7193767a49c0d0
Dimanskr <dimanskr2@gmail.com>
2025-01-13 13:44:16 +0300

    Add emlmployee and task models

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "rest_framework",
     # my apps
     "users",
+    "tracker",
 ]
 
 MIDDLEWARE = [
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "phone",
+        "city",
+        "avatar",
+        "tg_chat_id",
+    )
